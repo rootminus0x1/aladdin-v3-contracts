903151110e49a826c2c9bab856a77cdeddf37c2e
rootminus0x1 <rootminus0x1@gmail.com>
2023-09-30 18:10:58 +0100

    deleted non-f(x) stuff from contracts and test

--- contracts/clever/interfaces/ICommitUserSurrogate.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.7.6;
-
-interface ICommitUserSurrogate {
-  function commit(address _surrogate, address _contractAddr) external;
-}
--- contracts/concentrator/clever/interfaces/ICLeverAMOStrategy.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.7.6;
-
-import "../../strategies/ConcentratorStrategyBase.sol";
-
-interface ICLeverAMOStrategy is IConcentratorStrategy {
-  function strategyBalance() external view returns (uint256);
-}
