54018ff2120717583742567ba74f96a6d7aa7413
rootminus0x1 <rootminus0x1@gmail.com>
2024-02-02 18:05:22 +0000

    simplify arrays of variable values

--- test/eat.ts
+++ test/eat.ts
@@ -2,81 +2,39 @@ import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
 dotenvExpand.expand(dotenv.config());
 
-import { ContractWithAddress, contracts, delvePlot, deploy, getConfig, getSigner, writeEatFile } from 'eat';
-import { mermaid } from 'eat';
-import { asDateString } from 'eat';
-import { setupBlockchain } from 'eat';
+import { ContractWithAddress, contracts, deploy, getSigner, writeEatFile } from 'eat';
+import { getConfig, setupBlockchain } from 'eat';
 import { dig } from 'eat';
-import { delve } from 'eat';
+import { Values, valuesStepped, valuesSingle, delve, delvePlot } from 'eat';
 
 import { MockFxPriceOracle } from '@types';
 import { ethers } from 'hardhat';
 import { formatEther, parseEther } from 'ethers';
 
 async function main() {
-    // TODO: replace with a simple initialise function and add timestamp to config, etc
     await setupBlockchain();
 
     await dig();
-    await delve(); // no price change
 
-    // mock the price oracle
-    // TODO: hide deployer (make a global like whale, or maybe use whale?)
-    const deployer = await getSigner('deployer');
-    let oracle: ContractWithAddress<MockFxPriceOracle>;
-    oracle = await deploy<MockFxPriceOracle>('MockFxPriceOracle', deployer);
+    if (!getConfig().plot) {
+        await delve(); // no price change
+    }
 
+    // handle price changes
+    const oracle = await deploy<MockFxPriceOracle>('MockFxPriceOracle');
     await contracts.stETHTreasury.connect(contracts.stETHTreasury.ownerSigner).updatePriceOracle(oracle.address);
+    const setPrice = async (value: string) => {
+        await oracle.setPrice(parseEther(value));
+    };
 
     // TODO: handle multiple variables?
 
-    //const prices = Array.from({ length: 40 }, (_, index) => 4000 - index * 50);
-
-    // TODO: generalise this to take a function to set the new value
-    class PriceIterator {
-        private initial: number;
-        private done = false;
-        public value: string = '';
-
-        constructor(public name: string, private price: number, private finish?: number, private step: number = 1) {
-            this.initial = price;
-        }
-
-        private setPrice = async () => {
-            this.value = this.price.toString();
-            await oracle.setPrice(parseEther(this.value));
-        };
-
-        public next = async () => {
-            if (this.finish) {
-                // sequence
-                if (this.price < this.finish) {
-                    return undefined;
-                } else {
-                    await this.setPrice();
-                    this.price -= this.step;
-                    return this.value;
-                }
-            } else {
-                // single value
-                if (!this.done) {
-                    this.setPrice();
-                }
-            }
-        };
-
-        public reset = () => {
-            this.price = this.initial;
-            this.done = false;
-        };
-    }
-
     if (getConfig().plot) {
-        await delvePlot(new PriceIterator('ETH', 4000, 1010, 50), [
+        await delvePlot(await Values('ETH', valuesStepped(4000, 1010, -50), setPrice), [
             { contract: 'stETHTreasury', functions: ['collateralRatio', 'leverageRatio'] },
         ]);
     } else {
-        await delve(new PriceIterator('ETH', 2500)); // price change
+        await delve(await Values('ETH', valuesSingle(2500), setPrice)); // price change
     }
 }
 
--- test/results/aladdin-fx-market.CR_x_ETH-plot.gnuplot-script.gp
+++ test/results/aladdin-fx-market.CR_x_ETH-plot.gnuplot-script.gp
@@ -1,7 +1,7 @@
 datafile = "aladdin-fx-market.CR_x_ETH-plot.gnuplot.dat"
 # set terminal pngcairo
 # set output "aladdin-fx-market.CR_x_ETH-plot.gnuplot.png"
-# set terminal svg
+set terminal svg
 # set output "aladdin-fx-market.CR_x_ETH-plot.gnuplot.png"
 set xlabel "ETH"
 set ylabel "stETHTreasury.collateralRatio" # TODO: this should be the units, not the name
@@ -9,22 +9,22 @@ set ytics nomirror
 set y2label "stETHTreasury.leverageRatio"
 set y2tics
 
-stats datafile using 1 nooutput
-min = STATS_min
-max = STATS_max
-range_extension = 0.2 * (max - min)
-set xrange [min - range_extension : max + range_extension]
+#stats datafile using 1 nooutput
+#min = STATS_min
+#max = STATS_max
+#range_extension = 0.2 * (max - min)
+#set xrange [min - range_extension : max + range_extension]
 
-stats datafile using 2 nooutput
-min = STATS_min
-max = STATS_max
-range_extension = 0.2 * (max - min)
-set yrange [min - range_extension : max + range_extension]
+#stats datafile using 2 nooutput
+#min = STATS_min
+#max = STATS_max
+#range_extension = 0.2 * (max - min)
+#set yrange [min - range_extension : max + range_extension]
 
-stats datafile using 3 nooutput
-min = STATS_min
-max = STATS_max
-range_extension = 0.2 * (max - min)
-set y2range [min - range_extension : max + range_extension]
+#stats datafile using 3 nooutput
+#min = STATS_min
+#max = STATS_max
+#range_extension = 0.2 * (max - min)
+#set y2range [min - range_extension : max + range_extension]
 
 plot datafile using 1:2 with lines title "stETHTreasury.collateralRatio",     datafile using 1:3 with lines title "stETHTreasury.leverageRatio" axes x1y2
