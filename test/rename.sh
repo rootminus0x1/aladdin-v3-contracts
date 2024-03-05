fa258c66cc54bf246fee47652ca0f335430bc144
rootminus0x1 <rootminus0x1@gmail.com>
2024-03-05 14:50:08 +0000

    renamed config for shorter name

---
+++ test/rename.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <directory_path> <old_prefix> <new_prefix>"
+    exit 1
+fi
+
+directory_path="$1"
+old_prefix="$2"
+new_prefix="$3"
+
+cd "$directory_path" || exit 1
+
+for file in "$old_prefix"*; do
+    if [ -e "$file" ]; then
+        new_name="${file/$old_prefix/$new_prefix}"
+        mv "$file" "$new_name"
+        echo "Renamed: $file to $new_name"
+    fi
+done
